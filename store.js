3fce3358b1b9d16b19138d9169bd2f0bf2189d04
Egor Grishechko <egorgrishechko@gmail.com>
2022-04-26 20:56:12 +0200

    Main button

--- store.js
+++ store.js
@@ -68,13 +68,48 @@ var CakeStore = {
         var item_price = count * price;
         orderPriceEl.text(item_price);
 
-        // CakeStore.updateTotalPrice();
+        CakeStore.updateTotalPrice();
     },
 
-    getOrderItem: function(itemEl) {
+    getOrderItem: function (itemEl) {
         var id = itemEl.data('item-id');
-        return $('.js-order-item').filter(function() {
-          return ($(this).data('item-id') == id);
+        return $('.js-order-item').filter(function () {
+            return ($(this).data('item-id') == id);
         });
-      },
+    },
+    updateTotalPrice: function () {
+        var total_price = 0;
+        $('.js-item').each(function () {
+            var itemEl = $(this)
+            var price = +itemEl.data('item-price');
+            var count = +itemEl.data('item-count') || 0;
+            total_price += price * count;
+        });
+        CakeStore.canPay = total_price > 0;
+        CakeStore.totalPrice = total_price;
+        CakeStore.updateMainButton();
+    },
+    updateMainButton: function () {
+        var mainButton = Telegram.WebApp.MainButton;
+        if (CakeStore.modeOrder) {
+            if (CakeStore.isLoading) {
+                mainButton.setParams({
+                    is_visible: true,
+                    color: '#65c36d'
+                }).showProgress();
+            } else {
+                mainButton.setParams({
+                    is_visible: !!CakeStore.canPay,
+                    text: 'PAY ' + CakeStore.formatPrice(CakeStore.totalPrice),
+                    color: '#31b545'
+                }).hideProgress();
+            }
+        } else {
+            mainButton.setParams({
+                is_visible: !!CakeStore.canPay,
+                text: 'VIEW ORDER',
+                color: '#31b545'
+            }).hideProgress();
+        }
+    },
 }
